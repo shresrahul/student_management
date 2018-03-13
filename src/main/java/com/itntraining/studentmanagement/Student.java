a150ca45a7ce098178f68ae3dd5a4a9b7c24a8d8
Rahul <shresrahul@hotmail.com>
2018-03-13 08:40:11 +0545

    Initial Project Upload

---
+++ src/main/java/com/itntraining/studentmanagement/Student.java
@@ -0,0 +1,50 @@
+package com.itntraining.studentmanagement;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student_tbl")
+public class Student {
+	@Id //to make studentId Primary key
+	@GeneratedValue //auto increment
+	@Column(name="student_id")
+	private Long studentId;
+	
+	@Column(name="first_Name")
+	private String firstName;
+	@Column(name="last_Name")
+	private String lastName;
+	@Column(name="address")
+	private String address;
+	public Long getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
